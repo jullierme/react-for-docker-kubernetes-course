58663867c8f6c5481e0c50ff276bf55a199cc9f9
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-08-03 13:28:14 -0300

    change text name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Nice 
+          Learn React - DevOps is coming!
         </a>
       </header>
     </div>
